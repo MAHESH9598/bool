e925d5bce17207f362826af2fe24abee76fbbbc5
Mr. Mahesh Sunkara <46166792+MAHESH9598@users.noreply.github.com>
2019-01-15 19:34:49 +0530

    Initial commit

---
+++ bool.py
@@ -0,0 +1,23 @@
+#this is Example for ------------>bool
+
+""" Definition : --> bool is one of the class and treated as fundamental data type.
+
+                 --> The purpose of this data type is to store True (or) False.
+"""
+
+#### Examples :
+
+a=True
+print(a)
+
+b=False
+print(b)
+
+print("\n",type(b))
+
+print("\n a+b value is.... = ",a+b)
+
+print("\n a+a value is.... = ",a+a)
+
+print("\n a-b value is.... = ",a-b)
+
